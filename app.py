9f84ba9179c2fcc982fd7c29ab7a07519b58df0b
Johnny <huangjohnny22@gmail.com>
2026-05-18 18:31:24 -0400

    Added function to get station latitudes/longitudes

--- app.py
+++ app.py
@@ -1,1 +1,6 @@
 from helpers import *
+import streamlit as st
+
+st.title('Toronto Bike Share Availability')
+
+st.markdown('Need to rent a bike quickly but don\'t know where it\'s available? Say no more!')
